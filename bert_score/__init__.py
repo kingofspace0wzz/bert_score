365f4262d8950bb78112a2aa11e6a87532015ab1
Felix Wu <felixgwu@gmail.com>
2019-04-22 20:04:18 -0400

    remove visualization

--- bert_score/__init__.py
+++ bert_score/__init__.py
@@ -1,3 +1,3 @@
 __version__ = '0.1.0'
 from .utils import (get_idf_dict, bert_cos_score_idf,
-                    get_bert_embedding, plot_example, bert_types)
+                    get_bert_embedding, bert_types)
